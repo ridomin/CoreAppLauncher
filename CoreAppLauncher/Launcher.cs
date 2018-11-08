491c5677fabe54d0d25c133a66685d97863af45f
ridomin <ridomin@live.com>
2018-11-07 20:22:03 -0800

    Add project files.

---
+++ CoreAppLauncher/Launcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CoreAppLauncher
+{
+    public interface IIntializable
+    {
+        void InitializeComponent();
+    }
+
+    public class Launcher
+    {
+        public static void WPFLauncher<T>(T a) where T : System.Windows.Application, IIntializable
+        {
+            var baseTypeName = a.GetType().BaseType.Assembly.GetName().Name;
+            a.InitializeComponent();
+            var originalUri = a.StartupUri;
+            a.StartupUri = new Uri($"/{baseTypeName};component/{originalUri.ToString()}", UriKind.Relative);
+            a.Run();
+        }
+    }
+    
+}
